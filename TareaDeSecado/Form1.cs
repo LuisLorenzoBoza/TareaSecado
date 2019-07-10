bafd4b5c651a8971dea63529f02cfbf0f90f205e
luis lorenzo <sixsixonelui@gmail.com>
2019-07-10 11:36:17 -0400

    tarea de secado de cafe

--- TareaDeSecado/Form1.cs
+++ TareaDeSecado/Form1.cs
@@ -1,20 +1,129 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TareaDeSecado
 {
     public partial class Form1 : Form
     {
+        //  el secado del cafe 45 y 50 grados centigrados
+        //  entre 25 y 30 horas
+        //  El tiempo de secado sera 5 min como prueba el tiempo real son 25 a 30 horas
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        public void btnInicio_Click(object sender, EventArgs e)   /*inicio*/
+        {
+            timer1.Enabled = true;
+            timer1.Start();
+
+            int j = 0;
+            int m = 0;
+            Random rdn = new Random();
+            int a = rdn.Next(1, 5);
+            int x = 45;
+            j = x + a;
+            m = j;
+            tbtemperatura.Text = Convert.ToString(m);
+
+            //if (m > 45)
+            //{
+            //    do
+            //    {
+            //        compuerta(m);
+            //    }
+            //    while (m > 45);
+
+            //}
+            //else
+            ////{
+            //    tbtemperatura.Text = Convert.ToString(j);
+            //}
+
+        }
+
+        public void compuerta(int j)
+        {
+            for (int i = 0; i <= j; i--)
+                j--;
+            tbtemperatura.Text = Convert.ToString(value: j);
+        }
+
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        { }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int cont = 0;
+            do
+            {
+                pictureBox5.Left = pictureBox5.Left + 4;
+
+                if (pictureBox5.Location.X == 740)
+                {
+                    pictureBox5.Left = 0;
+                    cont++;
+                    timer2.Enabled = true;
+                }
+
+            }
+            while (cont == 1);
+        }
+
+        private void btnCafe_Click(object sender, EventArgs e)    /*cafe*/
+        {
+            pictureBox3.Image = Properties.Resources.grain_de_café_png;
+            int x = 45;
+            tbtemperatura.Text = Convert.ToString(x);
+            int y = 5000;
+            tbtiemposecado.Text = Convert.ToString(y);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int a = 0;
+            for (int i = 0; i <= 5; i++)
+            {
+                timer2.Interval += 10;
+                if (timer2.Interval == 50)
+                {
+                    for (int j = 0; j <= 3; j++)
+                    {
+                        a += 15;
+                        label7.Text = String.Empty;
+                        label7.Text = a.ToString();
+                    }
+                }
+            }
+
+            timer3.Enabled = true;
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            int a = 60;
+            for (int i = 0; i <= 5; i++)
+            {
+                timer3.Interval += 10;
+                if (timer3.Interval == 50)
+                {
+                    for (int j = 0; j <= 2; j++)
+                    {
+                        a -= 5;
+                        pictureBox7.Image = Properties.Resources.Fuego;
+                        label7.Text = String.Empty;
+                        label7.Text = a.ToString();
+                    }
+                }
+            }
         }
     }
 }
--- TareaDeSecado/Properties/Resources.Designer.cs
+++ TareaDeSecado/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TareaDeSecado.Properties
-{
-
-
+namespace TareaDeSecado.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,114 @@ namespace TareaDeSecado.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TareaDeSecado.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Caja {
+            get {
+                object obj = ResourceManager.GetObject("Caja", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Cerradura {
+            get {
+                object obj = ResourceManager.GetObject("Cerradura", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Fuego {
+            get {
+                object obj = ResourceManager.GetObject("Fuego", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap grain_de_café_png {
+            get {
+                object obj = ResourceManager.GetObject("grain-de-café-png", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap grain_de_café_png1 {
+            get {
+                object obj = ResourceManager.GetObject("grain-de-café-png1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Oregano_P {
+            get {
+                object obj = ResourceManager.GetObject("Oregano_P", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Oregano_P1 {
+            get {
+                object obj = ResourceManager.GetObject("Oregano_P1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
